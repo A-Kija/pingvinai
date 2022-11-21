3d01e0e15d8de46d3a5f27ffcdc2cb7c85b02bb1
A-Kija <arvydas.kijakauskas@gmail.com>
2022-11-21 11:35:08 +0200

    js task

---
+++ 001/app.js
@@ -0,0 +1,10 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+const a = [...Array(10)].map(() => getRandomIntInclusive(1, 11));
+
+
+console.log(a);

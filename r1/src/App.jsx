a9fb8c96a4b5c1b35d5961c74eac3471aae3c62f
A-Kija <arvydas.kijakauskas@gmail.com>
2022-11-28 10:17:25 +0200

    click

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.scss';
 
 function App() {
+
+
+  const now777 = (a) => {
+    console.log(a);
+    console.log('NOW is '+ a);
+  }
+
+
   return (
     <div className="App">
       <div className="App-header">
-      <button className="button-20">Button 20</button>
+        <button onClick={() => now777('9:00')}>Press on 9:00 PM</button>
+        <button onClick={() => now777('10:00')}>Press on 10:00 PM</button>
+        <button onClick={now777}>Press on 11:00 PM</button>
+        <button onClick={() => console.log('NOW!!!!!!')}>Press on 07:00 PM</button>
       </div>
     </div>
   );

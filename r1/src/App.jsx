f94b71177174229655cf82534eb1cab30eef194b
A-Kija <arvydas.kijakauskas@gmail.com>
2022-11-28 09:58:34 +0200

    button nice

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -3,7 +3,9 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      
+      <div className="App-header">
+      <button className="button-20">Button 20</button>
+      </div>
     </div>
   );
 }

b93a706b9b5f2481e1ae633905dab712b495017e
A-Kija <arvydas.kijakauskas@gmail.com>
2022-11-30 09:37:49 +0200

    kv

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,47 +1,23 @@
 import { useState } from 'react';
 import './App.scss';
-import randColor from './Functions/randColor';
-import rand from './Functions/rand';
-import Sq from './Components/006/Sq';
-// import Cat from './Components/006/Cat';
-// import Dog from './Components/006/Dog';
 
-// const m = [
-//     {name:'Pilkis', color: 'pink', type: 'cat'},
-//     {name:'Pūkis', color: 'crimson', type: 'cat'},
-//     {name:'Šarikas', color: 'gray', type: 'dog'},
-//     {name:'Rainis', color: 'skyblue', type: 'cat'},
-//     {name:'Bobikas', color: 'yellow', type: 'dog'}
-// ];
 
 function App() {
 
-    const [sq, setSq] = useState([]);
-
-    const add = () => {
-        setSq(s => [...s, 
-            {
-            id: rand(100000, 999999),
-            color: randColor()
-            }
-        ]);
-    }
-
+    const [ro, setRo] = useState(true);
 
     return (
         <div className="App">
             <div className="App-header">
-                {/* {m.map((a, i) => a.type === 'cat' ?
-            <Cat key={i} cat={a}/> :
-            <Dog key={i} dog={a}/>
-            )} */}
-            <div className="bin">
-                {
-                    sq.map(square => <Sq key={square.id} square={square} setSq={setSq} />)
-                }
+                <div className="kv" style={{
+                    transform: ro ? null : 'rotate(90deg)'
+                }}></div>
+                <div>
+                <button onClick={() => setRo(true)}>GO</button>
+                <button onClick={() => setRo(false)} style={{transform: 'rotate(90deg)'}}>GO</button>
+                </div>
             </div>
-                <button onClick={add}>ADD</button>
-            </div>
+            
         </div>
     );
 }

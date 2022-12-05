3b09ab73551dc9405c82f629dc61525877cfc671
A-Kija <arvydas.kijakauskas@gmail.com>
2022-12-05 11:40:43 +0200

    php

---
+++ 009/x.php
@@ -0,0 +1,22 @@
+<?php
+
+$trecias = 5;
+
+// echo ++$trecias;
+echo '<br>';
+// echo $trecias;
+echo '<br>';
+echo $trecias++ + ++$trecias;
+
+echo '<br>';
+echo $trecias += 10;
+
+$b = ' Jonas ';
+
+$pirmas = 'bla $b \n\n\n\n\n\n\n\n\n bla ';
+$antras = 'ku kū';
+$trecias = $pirmas.$antras;
+
+echo '<br>';
+echo $trecias;
+

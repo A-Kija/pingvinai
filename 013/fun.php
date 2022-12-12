fbb6ff890fc71dc85e829d40d3b24c36860cc2c7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-12-12 11:09:17 +0200

    recursija

--- 013/fun.php
+++ 013/fun.php
@@ -1,11 +1,11 @@
 <?php
 
-function sudeti(int $vienas, int $du) : int
+function sudeti($x, ...$a) : int
 {
     
-    $vienas = $vienas + $vienas;
+    // $vienas = $vienas + $vienas;
     
-    $rezultatas = $vienas + $du;
+    $rezultatas = $x + $a[0] + $a[1];
     return $rezultatas;
 }
 
@@ -16,6 +16,83 @@ $penki = 5;
 
 
   
-echo sudeti($vienas, $penki);
+echo sudeti($vienas, $penki, 88);
+echo '<pre><br>';
+// echo $vienas;
 echo '<br>';
-echo $vienas;
+
+function foo($reset = false) {
+    static $index = 0;
+    if ($reset) {
+        $index = 0;
+    } else {
+    $index++;
+    echo "$index\n";
+    }
+
+}
+
+// foo();
+// foo();
+// foo();
+// foo(true);
+// foo();
+// foo();
+// foo();
+
+echo '<br><br><br>';
+
+function recursive($num){
+    echo $num, '<br>';
+    if ($num < 3){
+        recursive($num + 1);
+    }
+    echo $num, '<br>';
+    return;
+}
+$startNum = 1;
+recursive($startNum);
+
+$mas = [
+    1,
+    1,
+    [
+        1,
+        1,
+        1,
+        [
+            1
+        ],
+        [
+            1, 
+            [
+                1,
+                1
+            ]
+        ]
+    ],
+    1,
+    1,
+    [
+        1
+    ]
+];
+
+print_r($mas);
+
+function sum(array $a) : int
+{
+    $s = 0;
+    foreach($a as $val) {
+        if (is_array($val)) {
+            $s += sum($val);
+        } else {
+            $s += $val;
+        }
+    }
+    echo "<h2>$s</h2>";
+    return $s;
+}
+
+echo '<br>';
+echo sum($mas);

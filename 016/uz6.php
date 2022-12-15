1382141dbad5d9ae4574d2e62a3fc7f2353e44a8
A-Kija <arvydas.kijakauskas@gmail.com>
2022-12-15 09:25:23 +0200

    3uz

---
+++ 016/uz6.php
@@ -0,0 +1,23 @@
+<?php
+
+function h1(string $text) : string
+{
+    return '<h1 style="display: inline; color: crimson;">'.$text.'</h1>';
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= preg_replace_callback('/(\d{2})(\d+)/', function($d) {
+    return h1($d[1]).$d[2];
+    }, md5(time())) ?>
+</body>
+</html>

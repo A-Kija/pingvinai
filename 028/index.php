4ce102ba92cee5b284820d9efc8b5fb479e545fa
A-Kija <arvydas.kijakauskas@gmail.com>
2023-01-09 11:34:32 +0200

    select

---
+++ 028/index.php
@@ -0,0 +1,36 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'miskas';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+// SKAITYMAS
+// SELECT column1, column2, ...
+// FROM table_name;
+
+$sql = "
+    SELECT id, title, height, type
+    FROM trees
+";
+
+$stmt = $pdo->query($sql);
+
+$trees = $stmt->fetchAll();
+
+echo '<pre>';
+
+print_r($trees);
+
+
+
+
